507c401f5c91e3ff87a34d475bc2c1549d1776cc
baptiste Dumont <baptiste21.dumont@gmail.com>
2026-09-14 11:33:31 +0200

    feat(phase-5): expose memory storage contract

--- src/jarvis/interfaces/__init__.py
+++ src/jarvis/interfaces/__init__.py
@@ -1,5 +1,6 @@
 """Stable interfaces exposed by JARVIS Enterprise."""
 
+from .memory_store import MemoryStore
 from .runtime import AgentRuntime
 
-__all__ = ["AgentRuntime"]
+__all__ = ["AgentRuntime", "MemoryStore"]
