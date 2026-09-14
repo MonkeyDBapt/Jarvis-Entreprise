4fec3aa6a804cca8fc1b84bbab64fdeda394e73d
baptiste Dumont <baptiste21.dumont@gmail.com>
2026-09-14 11:46:13 +0200

    feat(memory): expose context injection interface

--- src/jarvis/interfaces/__init__.py
+++ src/jarvis/interfaces/__init__.py
@@ -1,7 +1,15 @@
 """Stable interfaces exposed by JARVIS Enterprise."""
 
+from .memory_context import MemoryContext, MemoryContextInjector
 from .memory_retriever import MemoryRetriever, MemorySearchResult
 from .memory_store import MemoryStore
 from .runtime import AgentRuntime
 
-__all__ = ["AgentRuntime", "MemoryRetriever", "MemorySearchResult", "MemoryStore"]
+__all__ = [
+    "AgentRuntime",
+    "MemoryContext",
+    "MemoryContextInjector",
+    "MemoryRetriever",
+    "MemorySearchResult",
+    "MemoryStore",
+]
