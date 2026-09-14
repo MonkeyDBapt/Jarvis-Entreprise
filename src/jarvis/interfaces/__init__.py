db45508d6c377926327497170bfc281a21b97df2
baptiste Dumont <baptiste21.dumont@gmail.com>
2026-09-14 23:58:09 +0200

    feat(phase-5): expose memory lifecycle interface

--- src/jarvis/interfaces/__init__.py
+++ src/jarvis/interfaces/__init__.py
@@ -1,6 +1,7 @@
 """Stable interfaces exposed by JARVIS Enterprise."""
 
 from .memory_context import MemoryContext, MemoryContextInjector
+from .memory_lifecycle import MemoryLifecycle, MemoryLifecycleState
 from .memory_retriever import MemoryRetriever, MemorySearchResult
 from .memory_store import MemoryStore
 from .runtime import AgentRuntime
@@ -9,6 +10,8 @@ __all__ = [
     "AgentRuntime",
     "MemoryContext",
     "MemoryContextInjector",
+    "MemoryLifecycle",
+    "MemoryLifecycleState",
     "MemoryRetriever",
     "MemorySearchResult",
     "MemoryStore",
