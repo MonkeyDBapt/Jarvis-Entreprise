8ed0cc6a9baa2491bdd03efbbf64518e3b669959
baptiste Dumont <baptiste21.dumont@gmail.com>
2026-09-14 11:39:40 +0200

    feat(memory): expose retrieval interfaces

--- src/jarvis/interfaces/__init__.py
+++ src/jarvis/interfaces/__init__.py
@@ -1,6 +1,7 @@
 """Stable interfaces exposed by JARVIS Enterprise."""
 
+from .memory_retriever import MemoryRetriever, MemorySearchResult
 from .memory_store import MemoryStore
 from .runtime import AgentRuntime
 
-__all__ = ["AgentRuntime", "MemoryStore"]
+__all__ = ["AgentRuntime", "MemoryRetriever", "MemorySearchResult", "MemoryStore"]
