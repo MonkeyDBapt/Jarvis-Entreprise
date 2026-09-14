0abd1ea6b9ee29d2d7298fb0ca4bfc83cc496e80
baptiste Dumont <baptiste21.dumont@gmail.com>
2026-09-15 01:34:21 +0200

    feat(phase-6): expose model routing contracts

--- src/jarvis/intelligence/__init__.py
+++ src/jarvis/intelligence/__init__.py
@@ -3,5 +3,15 @@
 from .configuration import ModelConfiguration
 from .model import Model, ModelCapability, ModelType
 from .registry import ModelRegistry
+from .routing import ModelRouter, ModelSelectionRequest, ModelSelectionResult
 
-__all__ = ["Model", "ModelCapability", "ModelConfiguration", "ModelRegistry", "ModelType"]
+__all__ = [
+    "Model",
+    "ModelCapability",
+    "ModelConfiguration",
+    "ModelRegistry",
+    "ModelRouter",
+    "ModelSelectionRequest",
+    "ModelSelectionResult",
+    "ModelType",
+]
